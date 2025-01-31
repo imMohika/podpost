dd98633c5171b6e83de10e4cd7d1fd9af969b626
immohika <mohi222@pm.me>
2025-01-31 22:56:43 +0330

    feat: show active page in sidebar

--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -1,3 +1,4 @@
+import type { FileRouteTypes } from "@/routeTree.gen";
 import { Link } from "@tanstack/react-router";
 import {
   BoxesIcon,
@@ -18,13 +19,35 @@ import {
   Sidebar as UiSidebar,
 } from "./ui/sidebar";
 
+const navItems = [
+  {
+    link: "/",
+    title: "Home",
+    icon: <HomeIcon />,
+  },
+  {
+    link: "/library",
+    title: "Library",
+    icon: <LibraryIcon />,
+  },
+  {
+    link: "/tasks",
+    title: "Tasks",
+    icon: <BoxesIcon />,
+  },
+] as const satisfies Array<{
+  link: FileRouteTypes["to"];
+  title: string;
+  icon: React.ReactNode;
+}>;
+
 export const Sidebar = () => {
   return (
     <UiSidebar collapsible="icon">
       <SidebarHeader>
         <SidebarMenu>
           <SidebarMenuItem>
-            <div className="flex gap-2 pl-1 overflow-hidden items-center">
+            <div className="flex gap-2 pl-1 overflow-hidden items-center select-none">
               <div className="p-1 bg-purple-500 rounded-full w-fit text-neutral-50">
                 <PodcastIcon size={18} />
               </div>
@@ -37,30 +60,16 @@ export const Sidebar = () => {
         <SidebarGroup>
           <SidebarGroupContent>
             <SidebarMenu>
-              <SidebarMenuItem>
-                <SidebarMenuButton asChild>
-                  <Link to="/">
-                    <HomeIcon />
-                    <span>Home</span>
-                  </Link>
-                </SidebarMenuButton>
-              </SidebarMenuItem>
-              <SidebarMenuItem>
-                <SidebarMenuButton asChild>
-                  <Link to="/library">
-                    <LibraryIcon />
-                    <span>Library</span>
-                  </Link>
-                </SidebarMenuButton>
-              </SidebarMenuItem>
-              <SidebarMenuItem>
-                <SidebarMenuButton asChild>
-                  <Link to="/tasks">
-                    <BoxesIcon />
-                    <span>Tasks</span>
-                  </Link>
-                </SidebarMenuButton>
-              </SidebarMenuItem>
+              {navItems.map((item) => (
+                <SidebarMenuItem key={item.link}>
+                  <SidebarMenuButton asChild>
+                    <Link to={item.link} activeProps={{ "data-active": true }}>
+                      {item.icon}
+                      <span>{item.title}</span>
+                    </Link>
+                  </SidebarMenuButton>
+                </SidebarMenuItem>
+              ))}
             </SidebarMenu>
           </SidebarGroupContent>
         </SidebarGroup>
@@ -69,7 +78,7 @@ export const Sidebar = () => {
         <SidebarMenu>
           <SidebarMenuItem>
             <SidebarMenuButton asChild>
-              <Link to="/settings">
+              <Link to="/settings" activeProps={{ "data-active": true }}>
                 <SettingsIcon />
                 <span>Settings</span>
               </Link>
